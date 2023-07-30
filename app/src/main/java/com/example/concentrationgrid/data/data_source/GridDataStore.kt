b1ec45f044ebc5561e7b11fc1cdeb23e44e41a95
Declan <declanisaackuok@gmail.com>
2023-07-30 20:03:50 +0800

    Added datastore

--- app/src/main/java/com/example/concentrationgrid/data/data_source/GridDataStore.kt
+++ app/src/main/java/com/example/concentrationgrid/data/data_source/GridDataStore.kt
@@ -2,17 +2,34 @@ package com.example.concentrationgrid.data.data_source
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
 import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import com.example.concentrationgrid.GridSettings
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
 
+object GridSettingsSerializer: Serializer<GridSettings> {
+    override val defaultValue: GridSettings = GridSettings.getDefaultInstance()
 
-class GridDataStore(private var context: Context) {
-    fun obtainGridSettings(): SharedPreferences {
-        val gridSettingPreferenceName = "GridSettings"
-        return context.getSharedPreferences(gridSettingPreferenceName, Context.MODE_PRIVATE)
+    override suspend fun readFrom(input: InputStream): GridSettings {
+        try {
+            return GridSettings.parseFrom(input)
+
+        } catch(exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read from GridSettings ProtoDataStore: ", exception)
+        }
     }
+
+    override suspend fun writeTo(t: GridSettings, output: OutputStream) {
+        t.writeTo(output)
+    }
+
 }
 
-/*object GridSettingsSerializer: Serializer<GridSettings>() {
-    override val defaultValue: GridSettings
-        get() = TODO("Not yet implemented")
-}*/
+val Context.gridSettings: DataStore<GridSettings>  by dataStore(
+    fileName = "grid_settings.pb",
+    serializer = GridSettingsSerializer
+)
